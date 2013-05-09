33258924c0e04e1350658ae2c00ac560dab5f450
Eugen Sawin <esawin@me73.com>
2013-05-09 16:08:51 +0200

    Added first stringify tests and tuple support.

--- src/io/stringify.h
+++ src/io/stringify.h
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <string>
 #include <sstream>
+#include <tuple>
 #include <type_traits>
 #include <utility>
 
@@ -25,6 +26,14 @@ Output Convert(const Input& value) {
 // stream << operator.
 // TODO(esawin): Make thread-safe.
 struct Stringify {
+  static const std::string kDelim;
+  static const std::string kPairDiv;
+  static const std::string kWrapStart;
+  static const std::string kWrapEnd;
+  static const std::string kElemWrapStart;
+  static const std::string kElemWrapEnd;
+  static const std::string kStringWrap;
+
   static std::string delim;
   static std::string pair_div;
   static std::string wrap_start;
@@ -34,13 +43,21 @@ struct Stringify {
   static std::string string_wrap;
 };
 
-std::string Stringify::delim = ", ";
-std::string Stringify::pair_div = ": ";
-std::string Stringify::wrap_start = "(";
-std::string Stringify::wrap_end = ")";
-std::string Stringify::elem_wrap_start = "";
-std::string Stringify::elem_wrap_end = "";
-std::string Stringify::string_wrap = "";
+const std::string Stringify::kDelim = ", ";
+const std::string Stringify::kPairDiv = ": ";
+const std::string Stringify::kWrapStart = "(";
+const std::string Stringify::kWrapEnd = ")";
+const std::string Stringify::kElemWrapStart = "";
+const std::string Stringify::kElemWrapEnd = "";
+const std::string Stringify::kStringWrap = "\"";
+
+std::string Stringify::delim = Stringify::kDelim;
+std::string Stringify::pair_div = Stringify::kPairDiv;
+std::string Stringify::wrap_start = Stringify::kWrapStart;
+std::string Stringify::wrap_end = Stringify::kWrapEnd;
+std::string Stringify::elem_wrap_start = Stringify::kElemWrapStart;
+std::string Stringify::elem_wrap_end = Stringify::kElemWrapEnd;
+std::string Stringify::string_wrap = Stringify::kStringWrap;
 
 }  // namespace io
 }  // namespace flow
@@ -55,6 +72,16 @@ operator<<(std::ostream& stream, const Container<Args...>& con);
 template<typename T1, typename T2>
 std::ostream& operator<<(std::ostream& stream, const std::pair<T1, T2>& pair) {
   return stream << pair.first << flow::io::Stringify::pair_div << pair.second;
+}
+
+// Stream operator overload for tuples.
+template<typename... Tp>
+std::ostream& operator<<(std::ostream& stream, const std::tuple<Tp...>& t) {
+  return tuple_stream(t, stream, flow::io::Stringify::delim,
+      flow::io::Stringify::wrap_start, flow::io::Stringify::wrap_end,
+      flow::io::Stringify::pair_div,
+      flow::io::Stringify::elem_wrap_start, flow::io::Stringify::elem_wrap_end,
+      flow::io::Stringify::string_wrap);
 }
 
 template<template<typename...> class Container, typename... Args>
@@ -80,28 +107,28 @@ namespace io {
 // delimiter and wrapper.
 template<template<typename...> class Container, typename... Args>
 std::string Str(const Container<Args...>& con,
-    const std::string& delim = flow::io::Stringify::delim,
-    const std::string& wrap_start = flow::io::Stringify::wrap_start,
-    const std::string& wrap_end = flow::io::Stringify::wrap_end,
-    const std::string& pair_div = flow::io::Stringify::pair_div,
-    const std::string& elem_wrap_start = flow::io::Stringify::elem_wrap_start,
-    const std::string& elem_wrap_end = flow::io::Stringify::elem_wrap_end,
-    const std::string& string_wrap = flow::io::Stringify::string_wrap);
+    const std::string& delim = flow::io::Stringify::kDelim,
+    const std::string& wrap_start = flow::io::Stringify::kWrapStart,
+    const std::string& wrap_end = flow::io::Stringify::kWrapEnd,
+    const std::string& pair_div = flow::io::Stringify::kPairDiv,
+    const std::string& elem_wrap_start = flow::io::Stringify::kElemWrapStart,
+    const std::string& elem_wrap_end = flow::io::Stringify::kElemWrapEnd,
+    const std::string& string_wrap = flow::io::Stringify::kStringWrap);
 
 // Returns the string representation for the given value, args are ignored.
 template<typename T>
 std::string Str(const T& value,
-    const std::string& delim = flow::io::Stringify::delim,
-    const std::string& wrap_start = flow::io::Stringify::wrap_start,
-    const std::string& wrap_end = flow::io::Stringify::wrap_end,
-    const std::string& pair_div = flow::io::Stringify::pair_div,
-    const std::string& elem_wrap_start = flow::io::Stringify::elem_wrap_start,
-    const std::string& elem_wrap_end = flow::io::Stringify::elem_wrap_end,
-    const std::string& string_wrap = flow::io::Stringify::string_wrap) {
+    const std::string& delim = flow::io::Stringify::kDelim,
+    const std::string& wrap_start = flow::io::Stringify::kWrapStart,
+    const std::string& wrap_end = flow::io::Stringify::kWrapEnd,
+    const std::string& pair_div = flow::io::Stringify::kPairDiv,
+    const std::string& elem_wrap_start = flow::io::Stringify::kElemWrapStart,
+    const std::string& elem_wrap_end = flow::io::Stringify::kElemWrapEnd,
+    const std::string& string_wrap = flow::io::Stringify::kStringWrap) {
   return Convert<std::string>(value);
 }
 
-// Returns the string representation for the given string. Huh.
+// Returns the string representation for the given string.
 template<>
 std::string Str(const std::string& value, const std::string& delim,
     const std::string& wrap_start, const std::string& wrap_end,
@@ -115,18 +142,75 @@ std::string Str(const std::string& value, const std::string& delim,
 // delimiter, wrapper and pair delimiter.
 template<typename T1, typename T2>
 std::string Str(const std::pair<T1, T2>& pair,
-    const std::string& delim = flow::io::Stringify::delim,
-    const std::string& wrap_start = flow::io::Stringify::wrap_start,
-    const std::string& wrap_end = flow::io::Stringify::wrap_end,
-    const std::string& pair_div = flow::io::Stringify::pair_div,
-    const std::string& elem_wrap_start = flow::io::Stringify::elem_wrap_start,
-    const std::string& elem_wrap_end = flow::io::Stringify::elem_wrap_end,
-    const std::string& string_wrap = flow::io::Stringify::string_wrap) {
-  return Str(pair.first, delim, wrap_start, wrap_end, pair_div,
-             elem_wrap_start, elem_wrap_end, string_wrap) +
-    pair_div +
-    Str(pair.second, delim, wrap_start, wrap_end, pair_div,
-        elem_wrap_start, elem_wrap_end, string_wrap);
+    const std::string& delim = flow::io::Stringify::kDelim,
+    const std::string& wrap_start = flow::io::Stringify::kWrapStart,
+    const std::string& wrap_end = flow::io::Stringify::kWrapEnd,
+    const std::string& pair_div = flow::io::Stringify::kPairDiv,
+    const std::string& elem_wrap_start = flow::io::Stringify::kElemWrapStart,
+    const std::string& elem_wrap_end = flow::io::Stringify::kElemWrapEnd,
+    const std::string& string_wrap = flow::io::Stringify::kStringWrap) {
+  std::stringstream ss;
+  ss << wrap_start
+     << Str(pair.first, delim, wrap_start, wrap_end, pair_div,
+            elem_wrap_start, elem_wrap_end, string_wrap)
+     << pair_div
+     << Str(pair.second, delim, wrap_start, wrap_end, pair_div,
+            elem_wrap_start, elem_wrap_end, string_wrap)
+     << wrap_end;
+  return ss.str();
+}
+
+// Helper template for tuple iteration.
+template<size_t I = 0, typename... Tp>
+typename std::enable_if<I == sizeof...(Tp), std::ostream&>::type
+TupleStream(const std::tuple<Tp...>& t, std::ostream& stream,
+    const std::string& delim = flow::io::Stringify::kDelim,
+    const std::string& wrap_start = flow::io::Stringify::kWrapStart,
+    const std::string& wrap_end = flow::io::Stringify::kWrapEnd,
+    const std::string& pair_div = flow::io::Stringify::kPairDiv,
+    const std::string& elem_wrap_start = flow::io::Stringify::kElemWrapStart,
+    const std::string& elem_wrap_end = flow::io::Stringify::kElemWrapEnd,
+    const std::string& string_wrap = flow::io::Stringify::kStringWrap) {
+  return stream;
+}
+
+// Helper template for tuple iteration.
+template<size_t I = 0, typename... Tp>
+typename std::enable_if<I < sizeof...(Tp), std::ostream&>::type
+TupleStream(const std::tuple<Tp...>& t, std::ostream& stream,
+    const std::string& delim = flow::io::Stringify::kDelim,
+    const std::string& wrap_start = flow::io::Stringify::kWrapStart,
+    const std::string& wrap_end = flow::io::Stringify::kWrapEnd,
+    const std::string& pair_div = flow::io::Stringify::kPairDiv,
+    const std::string& elem_wrap_start = flow::io::Stringify::kElemWrapStart,
+    const std::string& elem_wrap_end = flow::io::Stringify::kElemWrapEnd,
+    const std::string& string_wrap = flow::io::Stringify::kStringWrap) {
+  if (I > 0) {
+    stream << delim;
+  }
+  stream << Str(std::get<I>(t), delim, wrap_start, wrap_end, pair_div,
+      elem_wrap_start, elem_wrap_end, string_wrap);
+  return TupleStream<I + 1, Tp...>(t, stream, delim, wrap_start, wrap_end,
+      pair_div, elem_wrap_start, elem_wrap_end, string_wrap);
+}
+
+// Returns the string representation for the given tuple based on provided
+// delimiter and wrappers.
+template<typename... Tp>
+std::string Str(const std::tuple<Tp...>& t,
+    const std::string& delim = flow::io::Stringify::kDelim,
+    const std::string& wrap_start = flow::io::Stringify::kWrapStart,
+    const std::string& wrap_end = flow::io::Stringify::kWrapEnd,
+    const std::string& pair_div = flow::io::Stringify::kPairDiv,
+    const std::string& elem_wrap_start = flow::io::Stringify::kElemWrapStart,
+    const std::string& elem_wrap_end = flow::io::Stringify::kElemWrapEnd,
+    const std::string& string_wrap = flow::io::Stringify::kStringWrap) {
+  std::stringstream stream;
+  stream << wrap_start;
+  TupleStream(t, stream, delim, wrap_start, wrap_end, pair_div,
+      elem_wrap_start, elem_wrap_end, string_wrap);
+  stream << wrap_end;
+  return stream.str();
 }
 
 template<template<typename...> class Container, typename... Args>
---
+++ src/test/stringify-test.cc
@@ -0,0 +1,110 @@
+// Copyright 2013 Eugen Sawin <esawin@me73.com>
+#include <gtest/gtest.h>
+// #include <gmock/gmock.h>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include "../io/stringify.h"
+
+using std::string;
+using std::stringstream;
+using std::vector;
+using std::tuple;
+using std::make_tuple;
+using std::make_pair;
+using std::pair;
+using std::map;
+using std::set;
+
+using namespace flow::io;  // NOLINT
+
+TEST(StringifyTest, vector) {
+  {
+    vector<int> vec = {};
+    EXPECT_EQ("()", Str(vec));
+  }
+  {
+    vector<int> vec = {1};
+    EXPECT_EQ("(1)", Str(vec));
+  }
+  {
+    vector<int> vec = {1, 2, 3, 4};
+    EXPECT_EQ("(1, 2, 3, 4)", Str(vec));
+  }
+  {
+    vector<string> vec = {"a", "b", "c"};
+    EXPECT_EQ("(\"a\", \"b\", \"c\")", Str(vec));
+    EXPECT_EQ("('a', 'b', 'c')",
+              Str(vec, ", ", "(", ")", "", "", "", "'"));
+  }
+  {
+    vector<int> vec = {1, 2, 3, 4};
+    EXPECT_EQ("[1; 2; 3; 4]", Str(vec, "; ", "[", "]"));
+  }
+  {
+    vector<int> vec = {};
+    stringstream ss;
+    ss << vec;
+    EXPECT_EQ("()", ss.str());
+  }
+  {
+    vector<int> vec = {1};
+    stringstream ss;
+    ss << vec;
+    EXPECT_EQ("(1)", ss.str());
+  }
+  {
+    vector<int> vec = {1, 2, 3, 4};
+    stringstream ss;
+    ss << vec;
+    EXPECT_EQ("(1, 2, 3, 4)", ss.str());
+  }
+  {
+    vector<int> vec = {1, 2, 3, 4};
+    stringstream ss;
+    Stringify::delim = "; ";
+    Stringify::wrap_start = "[";
+    Stringify::wrap_end = "]";
+    ss << vec;
+    EXPECT_EQ("[1; 2; 3; 4]", ss.str());
+  }
+  {
+    // TODO(esawin): Not supported, can not overload for string type without
+    // breaking the general operator use.
+    vector<string> vec = {"a", "b", "c"};
+    stringstream ss;
+    Stringify::delim = "; ";
+    Stringify::wrap_start = "[";
+    Stringify::wrap_end = "]";
+    Stringify::string_wrap = "'";
+    ss << vec;
+    // EXPECT_EQ("['a'; 'b'; 'c']", ss.str());
+  }
+}
+
+TEST(StringifyTest, tuple) {
+  {
+    tuple<string, int> t = make_tuple("a", 1);
+    EXPECT_EQ("(\"a\", 1)", Str(t)); 
+    EXPECT_EQ("['a'; 1]", Str(t, "; ", "[", "]", "", "", "", "'")); 
+  }
+  {
+    tuple<string, int, pair<string, int>> t =
+        make_tuple("a", 1, make_pair("b", 2));
+    EXPECT_EQ("(\"a\", 1, (\"b\": 2))", Str(t)); 
+  }
+}
+
+TEST(StringifyTest, map) {
+  {
+    map<string, int> m = {};
+    EXPECT_EQ("()", Str(m));
+  }
+  {
+    map<string, int> m = {{"a", 1}, {"b", 2}};
+    EXPECT_EQ("((\"a\": 1), (\"b\": 2))", Str(m));
+  }
+}
